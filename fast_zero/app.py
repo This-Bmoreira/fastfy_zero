b4f6e2df939ece9f71ec6457e9045b5eae4075be
This-Bmoreira <contatobrunosmoreira@gmail.com>
2024-07-08 19:20:07 -0300

    add HTTPStatus

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 
 from fastapi import FastAPI
 
@@ -6,6 +7,6 @@ from fast_zero.schemas import Message
 app = FastAPI()
 
 
-@app.get('/', response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo'}
