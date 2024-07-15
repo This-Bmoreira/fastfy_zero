b0ca3c0ff8d8e3331a2c917f82ddce4365dee92d
This-Bmoreira <contatobrunosmoreira@gmail.com>
2024-07-15 14:20:35 -0300

    add new test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,65 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+def test_read_root_should_ok_and_hello_wold(client):
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá mundo'}
 
 
-def test_read_root_should_ok_and_hello_wold():
-    client = TestClient(app)  # Organização do test
-    response = client.get('/')  # ação
-    assert response.status_code == HTTPStatus.OK  # assert
-    assert response.json() == {'message': 'Olá mundo'}  # assert
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'fake-user',
+            'password': 'fake-password',
+            'email': 'fake-email@test.com',
+        },
+    )  # ação
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'fake-user',
+        'email': 'fake-email@test.com',
+    }  # assert
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'fake-user',
+                'email': 'fake-email@test.com',
+            }
+        ]
+    }
+
+
+def test_update_users(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'id': 1,
+            'username': 'fake-user-update',
+            'password': 'fake-password',
+            'email': 'fake-email@test.com',
+        },
+    )
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'fake-user-update',
+        'email': 'fake-email@test.com',
+    }
+
+
+def test_delete_users(client):
+    response = client.delete(
+        '/users/1'
+    )
+
+    assert response.json() == {'message': 'User deleted'}
